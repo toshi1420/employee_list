17ccf108165b91669d34cdfc75710563c6dadc5e
t_ueno <toshi.scale@gmail.com>
2023-05-12 14:06:58 +0900

    flake8 エラー無視 noqa 追加

--- employee_list/settings/local.py
+++ employee_list/settings/local.py
@@ -1,11 +1,11 @@
-from .base import *
+from .base import *  # noqa: F403
 
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
-INSTALLED_APPS += ['debug_toolbar',]
+INSTALLED_APPS += ['debug_toolbar',]  # noqa: F405
 
-MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware',]
+MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware',]  # noqa: F405
 
-INTERNAL_IPS += ['127.0.0.1',]
+INTERNAL_IPS += ['127.0.0.1',]  # noqa: F405
--- employee_list/settings/production.py
+++ employee_list/settings/production.py
@@ -2,10 +2,10 @@ import os
 
 import environ
 
-from .base import *
+from .base import *  # noqa: F403
 
 env = environ.Env()
-env.read_env(os.path.join(BASE_DIR, '.env'))
+env.read_env(os.path.join(BASE_DIR, '.env'))  # noqa: F405
 
 DEBUG = env.bool("DEBUG")
 
